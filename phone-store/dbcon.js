1bdbbf5b3d8a38437646a0dbe1daac482e02a822
millelog <millelog@oregonstate.edu>
2019-02-28 22:50:41 -0800

    added Kuang's credentials to dbcon.js

--- phone-store/dbcon.js
+++ phone-store/dbcon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'hellouser',
-  password        : 'default',
-  database        : 'hellodb'
+  user            : 'cs340_kuangx',
+  password        : '1265',
+  database        : 'cs340_kuangx'
 });
 
 module.exports.pool = pool;
