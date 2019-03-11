388bd37f936a313b2785dcd018a36b9a1c7ee6ef
Unknown <kuangx@oregonstate.edu>
2019-03-11 05:29:00 -0700

    changed port numbre

--- phone-store-webapp/index.js
+++ phone-store-webapp/index.js
@@ -4,7 +4,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var mysql = require('./dbcon.js');
 
-var PORT = 8850;
+var PORT = 8849;
 
 //handlebars middleware
 app.engine('handlebars', handlebars.engine);
