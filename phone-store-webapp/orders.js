87b9f68465ca7565dd9560aaecc8d1c77b344e2e
millelog <millelog@oregonstate.edu>
2019-03-17 19:58:51 -0700

    Order creation page added

--- phone-store-webapp/orders.js
+++ phone-store-webapp/orders.js
@@ -15,6 +15,53 @@ module.exports = function(){
 			});
 	}
 
+	function getCustomers(res,mysql,context,complete){
+		mysql.pool.query("SELECT * FROM Customers",
+			(error,results,fields)=>{
+				if(error){
+					res.write(JSON.stringify(error));
+					res.end();
+				}
+				context.customers = results;
+				complete();
+		});
+	}
+
+	function getServicePlans(res,mysql,context,complete){
+		mysql.pool.query("SELECT * FROM ServicePlans",
+			(error,results,fields)=>{
+				if(error){
+					res.write(JSON.stringify(error));
+					res.end();
+				}
+				context.service_plans = results;
+				complete();
+		});
+	}
+
+	function getEmployees(res,mysql,context,complete){
+		mysql.pool.query("SELECT * FROM Employees",
+			(error,results,fields)=>{
+				if(error){
+					res.write(JSON.stringify(error));
+					res.end();
+				}
+				context.employees = results;
+				complete();
+		});
+	}
+
+	function getPhoneInfo(res,mysql,context,complete){
+		mysql.pool.query("SELECT DISTINCT Phones.IMEI, Phones_Infos.SKU, Phones_Infos.Manufacturer, Phones_Infos.Model, Phones_Infos.Phone_Name, Phones_Infos.Phone_Price FROM Phones LEFT JOIN Phones_Infos ON Phones.SKU=Phones_Infos.SKU",
+			(error,results,fields)=>{
+				if(error){
+					res.write(JSON.stringify(error));
+					res.end();
+				}
+				context.phones = results;
+				complete();
+		});
+	}
 
 	/*
 	//not sure if going to implement this function, removed the view button from the orders.handlebars file
@@ -35,7 +82,41 @@ module.exports = function(){
 			complete();
 		});
 	}
-	
+		
+	router.get('/add_order', (req,res)=>{
+		callbackCount = 0;
+		var context = {};
+		var mysql = req.app.get('mysql');
+
+		getCustomers(res,mysql,context,complete);
+		getServicePlans(res,mysql,context,complete);
+		getPhoneInfo(res,mysql,context,complete);
+		getEmployees(res,mysql,context,complete);
+
+		function complete(){
+			callbackCount++;
+			if(callbackCount >=4){
+				res.render('add-order', context);
+			}
+		}
+	});
+
+	router.post('/add_order', (req,res)=>{
+		var mysql = req.app.get('mysql');
+		var sql = "insert into Orders (Order_ID, Cust_ID, Emp_ID, Order_Date) values (?,?,?,?)";
+		var inserts = [req.body.Order_ID, req.body.Cust_ID, req.body.Emp_ID, req.body.Order_Date];
+		sql = mysql.pool.query(sql, inserts,(error,results,fields)=>{
+			if(error){
+				console.log(JSON.stringify(error))
+				res.write(JSON.stringify(error));
+				res.end();
+			}
+			else{
+				res.redirect('/orders');
+				}
+			});
+		});
+
 	/*
 		function that renders orders.handlebars
 		currently, the only javascript function is getOrders.
